181c1577124e7d59cd829affb46573f0a9e7e771
biswajeetkumar55 <biswajeetkumar55@gmail.com>
2023-06-27 23:40:16 +0530

    Bot 4 development in progess

---
+++ Scrape_Bot1/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Scrape_Bot1
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
